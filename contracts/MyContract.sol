8956eaee30dd1049dd3cc701224db16acdc44c5f
Sungyuk1 <kwonsun7@msu.edu>
2023-05-20 12:46:15 -0400

    Quick NFT hardhat tutorial

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -4,10 +4,12 @@ pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+//Quick hardhat ERC721 tutorial
+//https://www.youtube.com/watch?v=pQ4W1hZMp6c
 contract MyContract is ERC721{
 
     constructor(string memory name, string memory symbol) ERC721(name, symbol){
-        
+
 
     }
 
